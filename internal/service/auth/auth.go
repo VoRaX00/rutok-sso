c1f414b51b7297c0fc58d8dcc75c4d15c6bef895
nikita <nikitakerzhik@gmail.com>
2025-03-17 15:29:58 +1000

    init auth service

--- internal/service/auth/auth.go
+++ internal/service/auth/auth.go
@@ -1,9 +1,15 @@
 package auth
 
 import (
+	"context"
+	"github.com/google/uuid"
 	"log/slog"
 	"time"
 )
+
+type UserSaver interface {
+	Save(ctx context.Context, email string, password []byte) (uuid.UUID, error)
+}
 
 type Auth struct {
 	log      *slog.Logger
@@ -15,4 +21,16 @@ func NewAuth(log *slog.Logger, tokenTTL time.Duration) *Auth {
 		log:      log,
 		tokenTTL: tokenTTL,
 	}
+}
+
+func (a *Auth) Login(ctx context.Context, email, password string, appId int32) (string, error) {
+	panic("implement me")
+}
+
+func (a *Auth) Register(ctx context.Context, email, login, password string) (string, error) {
+	panic("implement me")
+}
+
+func (a *Auth) IsAdmin(ctx context.Context, email string) (bool, error) {
+	panic("implement me")
 }
